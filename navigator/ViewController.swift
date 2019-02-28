4a286bc950047a03d23e073783a7b1fcb51bca6e
luisfpinto <luisfpintos@gmail.com>
2019-02-28 13:58:30 +0100

    Example ready and working

--- navigator/ViewController.swift
+++ navigator/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var red: UISlider!
+    @IBOutlet weak var green: UISlider!
+    @IBOutlet weak var blue: UISlider!
+    @IBOutlet weak var redNumber: UILabel!
+    @IBOutlet weak var greenNumber: UILabel!
+    @IBOutlet weak var blueNumber: UILabel!
+    @IBOutlet weak var rectangle: UIView!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "openColor") {
+            let destination = segue.destination
+            destination.view.backgroundColor = rectangle.backgroundColor
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +34,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func updateColor() {
+        let redValue = CGFloat(red.value)
+        let greenValue = CGFloat(green.value)
+        let blueValue = CGFloat(blue.value)
+        redNumber.text = String(round(red.value*255))
+        greenNumber.text = String(round(green.value*255))
+        blueNumber.text = String(round(blue.value*255))
+//        view.backgroundColor = UIColor(displayP3Red: redValue, green: greenValue, blue: blueValue, alpha: 1)
+        rectangle.backgroundColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1)
+    }
+    
 }
 
